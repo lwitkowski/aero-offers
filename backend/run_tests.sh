51341b38a88597a1777b513a559711bccec6ce88
Łukasz Witkowski <contact@lukaszwitkowski.com>
2024-07-30 11:36:42 +0200

    Fixes

--- backend/run_tests.sh
+++ backend/run_tests.sh
@@ -1,21 +1,5 @@
 export PYTHONPATH=$PYTHONPATH':./'
 
-export DB_PORT=35432
-
-echo "Starting Postgres docker container"
-
-docker run \
-    --name test-db \
-    -d \
-    -e 'POSTGRES_DB=aircraft_offers' \
-    -e 'POSTGRES_USER=aircraft_offers' \
-    -e 'POSTGRES_PASSWORD=aircraft_offers' \
-    -p ${DB_PORT}':5432' \
-    -v ${PWD}'/../db/ddl.sql:/docker-entrypoint-initdb.d/ddl.sql' \
-    postgres:15-alpine
-
-sleep 5
-
 python3 -m unittest -f
 
 if [[ $? -ne 0 ]]; then
--- db/run_migrations_test.sh
+++ db/run_migrations_test.sh
@@ -1,11 +1,11 @@
 echo "Starting Postgres docker container"
 
-docker network create flyway-test-network
+docker network create test-network
 
 docker run --rm \
     --name test-db \
     -d \
-    --network flyway-test-network \
+    --network test-network \
     -e 'POSTGRES_DB=aircraft_offers' \
     -e 'POSTGRES_USER=aircraft_offers' \
     -e 'POSTGRES_PASSWORD=aircraft_offers' \
@@ -16,10 +16,10 @@ sleep 2
 echo "Running migrations"
 
 docker run --rm \
-    --network flyway-test-network \
+    --network test-network \
     -v ${PWD}'/migrations/:/flyway/sql' \
     flyway/flyway \
     -url=jdbc:postgresql://test-db:5432/aircraft_offers -user=aircraft_offers -password=aircraft_offers migrate
 
 docker rm -f test-db
-docker network rm flyway-test-network
+docker network rm test-network
