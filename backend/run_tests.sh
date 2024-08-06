85e0aaa7d646f890617e278cdb603ff950fe46cd
Łukasz Witkowski <contact@lukaszwitkowski.com>
2024-08-06 11:14:52 +0200

    Rise min coverage from 65 to 70

--- backend/run_tests.sh
+++ backend/run_tests.sh
@@ -3,4 +3,4 @@ export PYTHONPATH=$PYTHONPATH':./'
 set -e
 
 coverage run --source ./ --omit="tests/*" -m xmlrunner -o ./test-results
-coverage report --fail-under=65
+coverage report --fail-under=70
