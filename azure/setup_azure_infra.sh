a245c29d5032f4d6830a2172d903223f1fc46a53
Łukasz Witkowski <contact@lukaszwitkowski.com>
2025-02-13 09:38:28 +0100

    Migrate to GH Container Registry to optimize Azure costs (#132)

--- azure/setup_azure_infra.sh
+++ azure/setup_azure_infra.sh
@@ -1,7 +1,8 @@
 RESOURCE_GROUP=rg-aerooffers
 LOCATION="Switzerland North"
 ENV_NAME=cae-aerooffers-prod
-ACR=lwitkowski.azurecr.io
+CONTAINER_REGISTRY_SERVER=ghcr.io
+CONTAINER_REGISTRY=${CONTAINER_REGISTRY_SERVER}/lwitkowski
 DOCKER_IMAGE_TAG=e314458
 DB_HOST=localhost
 DB_PORT=5432
@@ -26,14 +27,12 @@ az identity create \
 # assign identity a 'Contributor' role to resource group  'rg-aerooffers'
 # create federated credential for identity with subject identifier repo:lwitkowski/aero-offers:environment:production
 
-docker login $ACR
-
 az containerapp create \
     --name ca-aerooffers-api \
     --resource-group $RESOURCE_GROUP \
     --environment $ENV_NAME \
-    --registry-server $ACR \
-    --image $ACR/aerooffers-api:$DOCKER_IMAGE_TAG \
+    --registry-server $CONTAINER_REGISTRY_SERVER \
+    --image $CONTAINER_REGISTRY/aerooffers-api:$DOCKER_IMAGE_TAG \
     --secrets "db-user=$DB_USER" "db-password=$DB_PASS" \
     --env-vars "DB_HOST=$DB_HOST" "DB_PORT=$DB_PORT" "DB_NAME=$DB_NAME" "DB_USER=secretref:db-user" "DB_PW=secretref:db-password" \
     --target-port 80 \
@@ -46,8 +45,8 @@ az containerapp create \
     --name ca-aerooffers-ui \
     --resource-group $RESOURCE_GROUP \
     --environment $ENV_NAME \
-    --registry-server $ACR \
-    --image $ACR/aerooffers-ui:$DOCKER_IMAGE_TAG \
+    --registry-server $CONTAINER_REGISTRY_SERVER \
+    --image $CONTAINER_REGISTRY/aerooffers-ui:$DOCKER_IMAGE_TAG \
     --target-port 80 \
     --cpu 0.25 --memory 0.5Gi \
     --min-replicas 1 --max-replicas 1 \
@@ -63,8 +62,8 @@ az containerapp job create \
     --trigger-type "Schedule" \
     --cron-expression "15 8,16 * * *" \
     --replica-timeout 1800 \
-    --registry-server $ACR \
-    --image $ACR/aerooffers-api:$DOCKER_IMAGE_TAG \
+    --registry-server $CONTAINER_REGISTRY_SERVER \
+    --image $CONTAINER_REGISTRY/aerooffers-api:$DOCKER_IMAGE_TAG \
     --secrets "db-user=$DB_USER" "db-password=$DB_PASS" \
     --env-vars "DB_HOST=$DB_HOST" "DB_PORT=$DB_PORT" "DB_NAME=$DB_NAME" "DB_USER=secretref:db-user" "DB_PW=secretref:db-password" \
     --command "sh" "./run_update_fx_rates.sh" \
@@ -77,8 +76,8 @@ az containerapp job create \
     --trigger-type "Schedule" \
     --cron-expression "17 3/11 * * *" \
     --replica-timeout 1800 \
-    --registry-server $ACR \
-    --image $ACR/aerooffers-api:$DOCKER_IMAGE_TAG \
+    --registry-server $CONTAINER_REGISTRY_SERVER \
+    --image $CONTAINER_REGISTRY/aerooffers-api:$DOCKER_IMAGE_TAG \
     --secrets "db-user=$DB_USER" "db-password=$DB_PASS" \
     --env-vars "DB_HOST=$DB_HOST" "DB_PORT=$DB_PORT" "DB_NAME=$DB_NAME" "DB_USER=secretref:db-user" "DB_PW=secretref:db-password" \
     --command "sh" "./run_update_offers.sh" \
