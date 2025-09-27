b117a61fb0e9871e5a604680c020fb2ad6daef1e
Łukasz Witkowski <contact@lukaszwitkowski.com>
2025-09-27 14:26:08 +0200

    Backend: added gunicorn config, changed port from 80 to 8080

--- backend/run_api_in_docker.sh
+++ backend/run_api_in_docker.sh
@@ -1,6 +1,6 @@
 docker build . -t aerooffers-local:latest
 docker run \
-  -p '8085:80' \
+  -p '8085:8080' \
   -e "COSMOSDB_URL=https://aerooffers-cosmosdb.documents.azure.com:443/" \
   -e "COSMOSDB_CREDENTIAL=??" \
   aerooffers-local:latest
