b9883e4c365d72cdc412d3071d5d72c6e1b6b87d
Łukasz Witkowski <contact@lukaszwitkowski.com>
2025-11-23 20:16:20 +0100

    Fixed model details view chart, tooltip and price stats (#302)

--- backend/run_api_in_docker.sh
+++ backend/run_api_in_docker.sh
@@ -1,6 +1,5 @@
 docker build . -t aerooffers-local:latest
 docker run \
   -p '8085:8080' \
-  -e "COSMOSDB_URL=https://aerooffers-cosmosdb.documents.azure.com:443/" \
-  -e "COSMOSDB_CREDENTIAL=??" \
+  --env-file ./.env.local \
   aerooffers-local:latest
